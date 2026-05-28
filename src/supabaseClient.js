d0bba4699e0bd045844c413c1bb6aa8d80d6eead
Matheus-Verdi <matheusverdifogaca2004@gmail.com>
2026-05-28 15:27:00 -0300

    teste

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Substitua com os dados que você acabou de copiar do seu painel do Supabase!
-const supabaseUrl = 'COLE_AQUI_O_SEU_PROJECT_URL';
-const supabaseAnonKey = 'COLE_AQUI_A_SUA_ANON_KEY';
+const supabaseUrl = 'https://hihhfjhxjxhovhzqpiua.supabase.co/rest/v1/';
+const supabaseAnonKey = 'sb_publishable_zRfZaBqu-kzKBfedI2jUGA_uC9estnp';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
